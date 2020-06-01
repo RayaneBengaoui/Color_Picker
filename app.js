f95206d68625ae11e7509b8f8c898450ca855ee2
Rayane Bengaoui <rayane.bengaoui@gmail.com>
2020-06-01 15:11:17 +0200

    updating sliders values

--- app.js
+++ app.js
@@ -48,6 +48,9 @@ function randomColors() {
 
     colorizeSliders(color, hue, brightness, saturation);
   });
+
+  //Reset inputs
+  resetInputs();
 }
 
 function checkTextContrast(color, text) {
@@ -116,6 +119,28 @@ function updatetextUI(index) {
   for (icon of icons) {
     checkTextContrast(color, icon);
   }
+}
+
+function resetInputs() {
+  const sliders = document.querySelectorAll(".sliders input");
+
+  sliders.forEach((slider) => {
+    if (slider.name === "hue") {
+      const hueColor = initialColors[slider.getAttribute("data-hue")];
+      const hueValue = chroma(hueColor).hsl()[0];
+      slider.value = Math.floor(hueValue);
+    }
+    if (slider.name === "brightness") {
+      const brightColor = initialColors[slider.getAttribute("data-bright")];
+      const brightValue = chroma(brightColor).hsl()[2];
+      slider.value = Math.floor(brightValue * 100) / 100;
+    }
+    if (slider.name === "saturation") {
+      const satColor = initialColors[slider.getAttribute("data-sat")];
+      const satValue = chroma(satColor).hsl()[1];
+      slider.value = Math.floor(satValue * 100) / 100;
+    }
+  });
 }
 
 //Function calls
